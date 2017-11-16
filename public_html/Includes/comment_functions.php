a8d261062712621fc5e67d06e02832396aa2fc65
Karim <>
2017-11-16 22:42:59 +0100

    Sem2 basic comments

--- public_html/Includes/comment_functions.php
+++ public_html/Includes/comment_functions.php
@@ -1,9 +1,13 @@
 <?php
 
-    function setComments()
+    function setComments($connection)
     {
         if(isset($_POST['send_comment']))
         {
+            $username = $_POST['username'];
+            $comment = $_POST['comment'];
             
+            $sql_instruction = "INSERT INTO comments (username, message) VALUES('$username', '$comment')";
+            $result = mysqli_query($connection, $sql_instruction);
         }
     }
